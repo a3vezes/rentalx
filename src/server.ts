0877875f6244e3171170f2624ae96be1eda1b46a
a3vezes <tucaariza@gmail.com>
2021-12-25 15:35:04 -0300

    Router Setup && Create Category

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import express, { Request, Response } from "express";
+import express from "express";
+
+import { categoriesRoutes } from "./routes/categories";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (request: Request, response: Response) => {
-  return response.json({ message: "Hello World Respawn" });
-});
+// Routes
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running"));
